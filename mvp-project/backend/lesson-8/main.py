038c49aab5d02ca6d27ca515cc6d4768bc22d604
adilet <adilet.issayev@gmail.com>
2024-11-20 11:20:34 -0600

    Update Endpoint

--- mvp-project/backend/lesson-8/main.py
+++ mvp-project/backend/lesson-8/main.py
@@ -4,8 +4,13 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to FastAPI behind Nginx"}
+    return {"message": "Welcome to FastAPI behind Nginx - This is Lesson 8 - Have Fun"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/lesson-8")
+def read_item(item_id: int, q: str = None):
+    return {"Congrats you completed Lesson 8 and now you know hot setup server and api"}
